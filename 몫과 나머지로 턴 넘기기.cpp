db609cac22b58371c4e2801a7d1da0b1e523160e
N図形⚓ <53482136+Nzukei@users.noreply.github.com>
2022-08-17 23:46:52 +0900

    Delete 몫과 나머지로 턴 넘기기.cpp

--- 몫과 나머지로 턴 넘기기.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main () {
-    int num=47, cnt=0;
-    while(num>=10) {
-        num=(num/10)*(num%10); // 몫과 나머지를 곱함(3번) (4*7=28, 2*8=16, 1*6=6)
-        cnt=cnt+1; // 1씩 더함
-    }
-    printf("%d", cnt);
-}
